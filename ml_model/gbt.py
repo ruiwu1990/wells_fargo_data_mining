1a68929f2f3df70396ea7076910b682ab7797330
ruiwu1990 <rui@cse.unr.edu>
2017-09-15 12:56:35 -0700

    add multi-class classifier

---
+++ ml_model/gbt.py
@@ -0,0 +1,27 @@
+from pyspark.ml.classification import LogisticRegression, OneVsRest
+from pyspark.ml.evaluation import MulticlassClassificationEvaluator
+
+# load data file.
+inputData = spark.read.format("libsvm") \
+	.load("data/mllib/sample_multiclass_classification_data.txt")
+
+total_accuracy = 0
+for i in range(20):
+	# generate the train/test split.
+	(train, test) = inputData.randomSplit([0.7, 0.3])
+	# instantiate the base classifier.
+	lr = LogisticRegression(maxIter=10, tol=1E-6, fitIntercept=True)
+	# instantiate the One Vs Rest Classifier.
+	ovr = OneVsRest(classifier=lr)
+	# train the multiclass model.
+	ovrModel = ovr.fit(train)
+	# score the model on test data.
+	predictions = ovrModel.transform(test)
+	# obtain evaluator.
+	evaluator = MulticlassClassificationEvaluator(metricName="accuracy")
+	# compute the classification error on test data.
+	accuracy = evaluator.evaluate(predictions)
+	# print("Test Error = %g" % (1.0 - accuracy))
+	total_accuracy = total_accuracy + accuracy
+
+print("Test Error = %g" % (1.0 - total_accuracy/20))
--- util.py
+++ util.py
@@ -6,8 +6,16 @@ import csv
 import os
 from collections import defaultdict
 import json
+import shutil
+import numpy as np
+from datetime import datetime
 
-app_path = os.path.dirname(os.path.abspath(__file__))
+import matplotlib.pyplot as plt
+from matplotlib import cm
+from matplotlib.ticker import LinearLocator, FormatStrFormatter
+import matplotlib.mlab as mlab
+
+app_path = os.path.dirname(os.path.abspath('__file__'))
 
 def get_predict_observed(filename,predicted_name,observed_name):
 	'''
@@ -21,3 +29,75 @@ def get_predict_observed(filename,predicted_name,observed_name):
 	predicted_list = predicted.tolist()
 	observed_list = observed.tolist()
 	return [list(a) for a in zip(predicted_list, observed_list)]
+
+# this will be '/cse/home/rwu/Desktop/machine_learning_prms_accuracy/tmp_test'
+# /cse/home/rwu/Desktop/hadoop/spark_installation/spark-2.1.0/bin/pyspark
+
+# the following construct_line and convert_csv_into_libsvm
+# convert csv into libsvm
+# the function is basically 
+# from https://github.com/zygmuntz/phraug/blob/master/csv2libsvm.py
+def construct_line( label, line ):
+	new_line = []
+	if float( label ) == 0.0:
+		label = "0"
+	new_line.append( label )
+
+	for i, item in enumerate( line ):
+		if item == '' or float( item ) == 0.0:
+			continue
+		new_item = "%s:%s" % ( i + 1, item )
+		new_line.append( new_item )
+	new_line = " ".join( new_line )
+	new_line += "\n"
+	return new_line
+
+def convert_csv_into_libsvm(input_file,output_file,label_index=0,skip_headers=True):
+	'''
+	the function converts csv into libsvm
+	'''
+	i = open( input_file, 'rb' )
+	o = open( output_file, 'wb' )
+	reader = csv.reader( i )
+
+	if skip_headers:
+		headers = reader.next()
+
+	for line in reader:
+		if label_index == -1:
+			label = '1'
+		else:
+			label = line.pop( label_index )
+
+		new_line = construct_line( label, line )
+		o.write( new_line )
+
+'''
+categories
+1 [0,2000)
+2 [2000,4000)
+3 [4000,6000)
+4 [6000,8000)
+5 [8000,10000)
+6 [10000, inf]
+'''
+total_len = df.shape[0]
+df['sav_cat'] = pd.Series(np.random.randn(total_len), index=df.index)
+
+for i in range(total_len):
+	# remove 2016 from time
+	df['asof_yyyymm'][i] = df['asof_yyyymm'][i]-201600
+	if df['sav_bal_altered'][i]>=0 and df['sav_bal_altered'][i]<2000:
+		df['sav_cat'][i] = 1
+	elif df['sav_bal_altered'][i]>=2000 and df['sav_bal_altered'][i]<4000:
+		df['sav_cat'][i] = 2
+	elif df['sav_bal_altered'][i]>=4000 and df['sav_bal_altered'][i]<6000:
+		df['sav_cat'][i] = 3
+	elif df['sav_bal_altered'][i]>=6000 and df['sav_bal_altered'][i]<8000:
+		df['sav_cat'][i] = 4
+	elif df['sav_bal_altered'][i]>=8000 and df['sav_bal_altered'][i]<10000:
+		df['sav_cat'][i] = 5
+	else:
+		df['sav_cat'][i] = 6
+
+df.to_csv(path_or_buf='test.csv')
